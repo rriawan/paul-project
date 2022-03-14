5bdc7759f0e4e47f5257fdb947a807a4d5e9c5d2
rriawan <riawanrahmat@gmail.com>
2022-03-14 17:00:46 +0700

    update 14/3/2022 sore

--- application/views/admin_manage-users.php
+++ application/views/admin_manage-users.php
@@ -27,23 +27,46 @@
 									$row->IsActive = "Non - Active";
 								}
 								?>
-								<tr>
-									<td><?= $row->Name ?></td>
-									<td><?= $row->Username ?></td>
-									<td><?= $row->Email ?></td>
-									<td><?= $row->PhoneNumber ?></td>
-									<td><?= $row->IsActive ?></td>
-									<td><?= $row->CreateAt ?></td>
-								</tr>
+							<tr>
+								<td>
+									<a href="#" data-toggle="modal" data-target="#modal-default">
+										<strong><?= $row->Name ?></strong>
+									</a>
+								</td>
+								<td><?= $row->Username ?></td>
+								<td><?= $row->Email ?></td>
+								<td><?= $row->PhoneNumber ?></td>
+								<td><?= $row->IsActive ?></td>
+								<td><?= $row->CreateAt ?></td>
+							</tr>
 							<?php } ?>
 						</tbody>
-						
+
 					</table>
 				</div>
 				<!-- /.card-body -->
-			
+
 			</div>
 			<!-- /.card -->
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="modal-default">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Default Modal</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<p>One fine body&hellip;</p>
+			</div>
+			<div class="modal-footer justify-content-between">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
 		</div>
 	</div>
 </div>
